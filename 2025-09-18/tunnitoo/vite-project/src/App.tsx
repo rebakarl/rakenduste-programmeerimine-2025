1adfb29e0d9de9862edc47e6faf4e2ca0ec1719a
Karl <krebane@tlu.ee>
2025-09-23 20:42:32 +0300

    feat: added Themeprovider

--- 2025-09-18/tunnitoo/vite-project/src/App.tsx
+++ 2025-09-18/tunnitoo/vite-project/src/App.tsx
@@ -2,9 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import About from './Components/About'
-import Home from './Components/Home'
-import Something from './Components/Something'
 
 function App() {
   const [count, setCount] = useState(0)
--- 2025-09-18/tunnitoo/vite-project/src/main.tsx
+++ 2025-09-18/tunnitoo/vite-project/src/main.tsx
@@ -3,11 +3,20 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
-
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#e6a519ff', // värv
+    },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>,
 );
